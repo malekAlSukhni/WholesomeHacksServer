d1c130f830878f04fb56625203482860722c5d1a
malekAlSukhni <malek71397@gmail.com>
2018-03-31 18:54:45 -0500

    adding pom dependencies

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -3,18 +3,36 @@ package hello;
 public class Greeting {
 
     private final long id;
-    private final String content;
+    private final String name;
+    private final String item;
+    private final String size;
+    private final String price;
 
-    public Greeting(long id, String content) {
+    public Greeting(long id, String name, String item, String size, String price) {
         this.id = id;
-        this.content = content;
+        this.name = name;
+        this.item = item;
+        this.size = size;
+        this.price = price;
     }
 
     public long getId() {
         return id;
     }
 
-    public String getContent() {
-        return content;
+    public String getName() {
+        return name;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getPrice() {
+        return price;
     }
 }
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -1,5 +1,8 @@
 package hello;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +14,105 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    private static final int ID = 0;
+    private static final int NAME = 1;
+    private static final int TYPE = 1;
+    private static final int ITEM = 2;
+    private static final int SIZE = 3;
+    private static final int PRICE = 4;
+    private static final int CURRENT = 5;
+
+    HashMap<String, ArrayList<ArrayList<String>>> globalMapByType = new HashMap();
+    HashMap<String, ArrayList<ArrayList<String>>> globalMapByOwner = new HashMap();
+    HashMap<String, ArrayList<ArrayList<String>>> globalMapByOwned = new HashMap();
+
+    @RequestMapping("/get")
+    public ArrayList<Greeting> greetingget(@RequestParam(value="type", defaultValue="World") String type) {
+        return doGet("list", type);
+    }
+
+    @RequestMapping("/put")
+    public Boolean greetingput(@RequestParam(value="name", defaultValue="World") String name,
+                               @RequestParam(value="item", defaultValue="World") String item,
+                               @RequestParam(value="type", defaultValue="World") String type,
+                               @RequestParam(value="size", defaultValue="World") String size,
+                               @RequestParam(value="price", defaultValue="World") String price) {
+        return doPut(name, item, type, size, price);
+    }
+
+    public synchronized ArrayList<Greeting> doGet(String reqType, String type) {
+
+        ArrayList<Greeting> finalreturn = new ArrayList<>();
+
+        switch(reqType){
+            case "list":
+                String itemType = type;
+                ArrayList<ArrayList<String>> list = globalMapByType.get(itemType);
+                if(list == null){
+                    break;
+                }
+                for(ArrayList<String> a : list) {
+                    Greeting g = new Greeting(Long.parseLong(a.get(ID)), a.get(NAME), a.get(ITEM), a.get(SIZE), a.get(PRICE));
+                    finalreturn.add(g);
+                }
+        }
+
+        return finalreturn;
+    }
+
+    public synchronized boolean doPut(String name, String item, String type, String size, String price) {
+
+        String currentOwner = "";
+        String id = Integer.toString((int) counter.incrementAndGet());
+
+        if(globalMapByType.get(type) != null){
+            ArrayList<ArrayList<String>> listOfItems = globalMapByType.get(type);
+            ArrayList<String> info = new ArrayList<>();
+            info.add(id);
+            info.add(name);
+            info.add(item);
+            info.add(size);
+            info.add(price);
+            info.add(currentOwner);
+            listOfItems.add(info);
+            globalMapByType.put(type, listOfItems);
+        } else {
+            ArrayList<ArrayList<String>> newListOfItems = new ArrayList<>();
+            ArrayList<String> info = new ArrayList<>();
+            info.add(id);
+            info.add(name);
+            info.add(item);
+            info.add(size);
+            info.add(price);
+            info.add(currentOwner);
+            newListOfItems.add(info);
+            globalMapByType.put(type, newListOfItems);
+        }
+
+        if(globalMapByOwner.get(name) != null){
+            ArrayList<ArrayList<String>> listOfItems = globalMapByOwner.get(name);
+            ArrayList<String> info = new ArrayList<>();
+            info.add(id);
+            info.add(type);
+            info.add(item);
+            info.add(size);
+            info.add(price);
+            info.add(currentOwner);
+            listOfItems.add(info);
+            globalMapByOwner.put(name, listOfItems);
+        } else {
+            ArrayList<ArrayList<String>> newListOfItems = new ArrayList<>();
+            ArrayList<String> info = new ArrayList<>();
+            info.add(id);
+            info.add(type);
+            info.add(item);
+            info.add(size);
+            info.add(price);
+            info.add(currentOwner);
+            newListOfItems.add(info);
+            globalMapByOwner.put(name, newListOfItems);
+        }
+
+        return true;
     }
 }
